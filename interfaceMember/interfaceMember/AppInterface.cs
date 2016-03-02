9a5a54ecdd357b8f3f5c0cb8a3110b7ea3b9529c
unknown <kagnihotri@Crux.internal.tavisca.com>
2016-03-02 11:45:03 +0530

    test

---
+++ interfaceMember/interfaceMember/AppInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace interfaceMember
+{
+    
+
+    interface AppInterface
+{
+        // Property signatures:
+        int x
+        {
+            get;
+            set;
+        }
+
+        int y
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ interfaceMember/interfaceMember/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace interfaceMember
+{
+    class Program
+    {
+        static void PrintPoint(AppInterface p)
+        {
+            Console.WriteLine(p.GetType());
+            Console.WriteLine("Number 1 ={0}, Number 2 ={1}", p.x, p.y);
+        }
+
+        static void Main()
+        {
+            Addition p = new Addition(20, 30);
+            PrintPoint(p);
+            Console.ReadLine();
+        }
+    }
+
+    class Addition : AppInterface
+    {
+        private int _num1;
+        private int _num2;
+
+        public Addition(int x, int y)
+        {
+            _num1 = x + y;
+            _num2 = y - x;
+        }
+
+        public int x
+        {
+            get { return _num1; }
+            set { _num1 = value; }
+        }
+
+        public int y
+        {
+            get { return _num2; }
+            set { _num2 = value; }
+        }
+    }
+}
